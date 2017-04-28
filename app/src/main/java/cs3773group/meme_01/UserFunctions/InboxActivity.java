f01932f02a57fa3354d0cb295787df5fc2c011ba
Sean <seanwoerner.ru89@gmail.com>
2017-04-28 15:56:02 -0500

    Added dynamic list stuff

--- app/src/main/java/cs3773group/meme_01/UserFunctions/InboxActivity.java
+++ app/src/main/java/cs3773group/meme_01/UserFunctions/InboxActivity.java
@@ -1,9 +1,17 @@
 package cs3773group.meme_01.UserFunctions;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
+import cs3773group.meme_01.MainActivity;
 import cs3773group.meme_01.R;
 
 /**
@@ -11,15 +19,54 @@ import cs3773group.meme_01.R;
  */
 
 public class InboxActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button bSelectUser;
+    private Button bBack;
+    private RadioGroup messageList;
+    private RadioButton messageButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_inbox);
+
+        bSelectUser = (Button) findViewById(R.id.bSelectUser);
+        bSelectUser.setOnClickListener(this);
+        bBack = (Button) findViewById(R.id.bBack);
+        bBack.setOnClickListener(this);
+
+        final TextView logoutLink = (TextView) findViewById(R.id.linkLogout);
+        logoutLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(InboxActivity.this, MainActivity.class);
+                InboxActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        Intent intent = getIntent();
+
+        // REPLACE 0 WITH listsz
+        createMessageList(0);
     }
 
     @Override
     public void onClick(View v){
 
+    }
+
+    public void createMessageList(int listLength) {
+        messageList = new RadioGroup(this);
+        messageList.setOrientation(LinearLayout.VERTICAL);
+
+        for (int i = 0; i < listLength; i++) {
+            RadioButton rbMessage = new RadioButton(this);
+
+            // REPLACE messageList.getID() with stuff
+            rbMessage.setId(i);
+            rbMessage.setText(messageList.getId());
+            messageList.addView(rbMessage);
+        }
+        ((ViewGroup) findViewById(R.id.listUsers)).addView(messageList);
     }
 }
