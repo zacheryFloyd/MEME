2b7e663fd7cb3841534e27ba9e80e6f5ebb34c50
Tom <user 1@172.17.105.98>
2017-04-28 15:59:59 -0500

    added functionality to message and view inbox

--- app/src/main/java/cs3773group/meme_01/UserFunctions/InboxActivity.java
+++ app/src/main/java/cs3773group/meme_01/UserFunctions/InboxActivity.java
@@ -4,16 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-<<<<<<< Updated upstream
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-=======
 import android.widget.Button;
->>>>>>> Stashed changes
 
 import cs3773group.meme_01.MainActivity;
 import cs3773group.meme_01.R;
@@ -23,36 +20,21 @@ import cs3773group.meme_01.R;
  */
 
 public class InboxActivity extends AppCompatActivity implements View.OnClickListener {
-<<<<<<< Updated upstream
-    private Button bSelectUser;
-    private Button bBack;
     private RadioGroup messageList;
     private RadioButton messageButton;
-
-=======
     private Button bViewMessage;
     private Button bDeleteMessage;
     private Button bBack;
->>>>>>> Stashed changes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_inbox);
 
-        bSelectUser = (Button) findViewById(R.id.bSelectUser);
-        bSelectUser.setOnClickListener(this);
+        bViewMessage = (Button) findViewById(R.id.bViewMessage);
+        bViewMessage.setOnClickListener(this);
         bBack = (Button) findViewById(R.id.bBack);
         bBack.setOnClickListener(this);
-
-        final TextView logoutLink = (TextView) findViewById(R.id.linkLogout);
-        logoutLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent registerIntent = new Intent(InboxActivity.this, MainActivity.class);
-                InboxActivity.this.startActivity(registerIntent);
-            }
-        });
 
         Intent intent = getIntent();
 
@@ -74,7 +56,7 @@ public class InboxActivity extends AppCompatActivity implements View.OnClickList
 
             // REPLACE messageList.getID() with stuff
             rbMessage.setId(i);
-            rbMessage.setText(messageList.getId());
+          //  rbMessage.setText(messageList.getId());
             messageList.addView(rbMessage);
         }
         ((ViewGroup) findViewById(R.id.listUsers)).addView(messageList);
